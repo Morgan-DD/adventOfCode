a203101a5ee73c70f1e11461890500655fbcb2e7
Morgan-DD <111990474+Morgan-DD@users.noreply.github.com>
2024-03-04 17:04:47 +0100

    done 2.1 -> start 1.2

--- 01/adventOfCode01.py
+++ 01/adventOfCode01.py
@@ -1,7 +1,7 @@
-import shutil
-import platform
+#
+# Advent of code 01
+#
 import os
-import glob
 import sys
 import re
 script_folder_path = os.path.dirname(os.path.abspath(sys.argv[0]))
@@ -9,9 +9,30 @@ dataFilePath = script_folder_path + "\\data.txt"
 datas = open(dataFilePath).read()
 finalNumber=0
 
+# part one
 datas=datas.split("\n")
 for data in datas:
     number = re.sub("[A-z]","",data)
     number = int(number[0]) * 10 + int(number[-1])
     finalNumber=finalNumber+number
+print(finalNumber)
+
+#part two
+finalNumber=0
+for data in datas:
+    print(data)
+    data=data.replace("one", "1")
+    data=data.replace("two", "2")
+    data=data.replace("three", "3")
+    data=data.replace("four", "4")
+    data=data.replace("five", "5")
+    data=data.replace("six", "6")
+    data=data.replace("seven", "7")
+    data=data.replace("eight", "8")
+    data=data.replace("nine", "9")
+    number = re.sub("[A-z]","",data)
+    number = int(number[0]) * 10 + int(number[-1])
+    print(number)
+    finalNumber=finalNumber+number
+print("----------")
 print(finalNumber)
