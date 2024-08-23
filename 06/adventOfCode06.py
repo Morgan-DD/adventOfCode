30322d6f990e270e29c3752e2c038b07d5391ed9
Morgan Dimitri Dussault <111990474+Morgan-DD@users.noreply.github.com>
2024-08-23 16:34:28 +0200

    start of lvl 6.1

--- 06/adventOfCode06.py
+++ 06/adventOfCode06.py
@@ -1,11 +1,19 @@
 #
 # Advent of code 06
 #
-import os
-import sys
-import re
-import numpy 
-script_folder_path = os.path.dirname(os.path.abspath(sys.argv[0]))
-lineFilePath = script_folder_path + "\\data.txt"
-fileContent = open(lineFilePath).read()
-total = 0
+time = [63, 78, 94, 68]
+distance = [411, 1274, 2047, 1035]
+
+totalPosibilitys = 0
+
+idRace = 0
+possibility = 0
+for raceTime in time:
+    raceDistance = distance[idRace]
+    print(str(raceTime) + " | " +  str(raceDistance))
+    idRace+=1
+    for i in range(raceTime):
+        if(i*(raceTime-i) > raceDistance):
+            print(str(i) + " * " + str(raceTime-i))
+    if(possibility)
+    possibility = 0
