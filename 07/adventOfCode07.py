1c94dbca328a4c67a2547795b339fbbc2cd26ae4
Morgan-DD <111990474+Morgan-DD@users.noreply.github.com>
2024-08-28 13:51:44 +0200

    end 8.1

--- 07/adventOfCode07.py
+++ 07/adventOfCode07.py
@@ -3,7 +3,7 @@
 #
 import os
 import sys
-from collections import Counter
+
 script_folder_path = os.path.dirname(os.path.abspath(sys.argv[0]))
 lineFilePath = script_folder_path + "\\data.txt"
 fileContent = open(lineFilePath).read()
@@ -18,11 +18,11 @@ def getCharNumberInString(string, charSearched):
             counter+=1
     return counter
 
-def getEntryValue(entry):
+def getEntryValue(entryToGetValueFrom):
 #    print(entry)
-    for char in entry:
+    for char in entryToGetValueFrom:
         print("-# " + char)
-        print(getCharNumberInString(entry, char))
+        print(getCharNumberInString(entryToGetValueFrom, char))
 
 
 for line in fileContent.split("\n"):
