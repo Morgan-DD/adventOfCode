9ec11dac9b59b2f8af505304556d4454dadd4f38
Morgan Dimitri Dussault <111990474+Morgan-DD@users.noreply.github.com>
2024-08-29 08:11:25 +0200

    merge

--- 07/adventOfCode07.py
+++ 07/adventOfCode07.py
@@ -7,6 +7,7 @@ import sys
 script_folder_path = os.path.dirname(os.path.abspath(sys.argv[0]))
 lineFilePath = script_folder_path + "\\data.txt"
 fileContent = open(lineFilePath).read()
+rolls = [[], [], [], [], [], [], []]
 total = 0
 entry = 0
 number = 0
@@ -18,14 +19,54 @@ def getCharNumberInString(string, charSearched):
             counter+=1
     return counter
 
-def getEntryValue(entryToGetValueFrom):
+def getEntryValue(entry):
 #    print(entry)
-    for char in entryToGetValueFrom:
-        print("-# " + char)
-        print(getCharNumberInString(entryToGetValueFrom, char))
+    char = entry[0]
+    entryModify = entry
+   # print("-# " + char)
+    match getCharNumberInString(entry, char):
+        case 5:
+            rolls[0].append([entry, number]) 
+        case 4:
+            rolls[1].append([entry, number]) 
+        case 3:
+            if(getCharNumberInString(entryModify.replace(char, ""), entryModify.replace(char, "")[0]) == 2):
+                rolls[2].append([entry, number]) 
+            else:
+                rolls[3].append([entry, number]) 
+        case 2:
+            if(getCharNumberInString(entryModify.replace(char, ""), entryModify.replace(char, "")[0]) == 3):
+                rolls[2].append([entry, number]) 
+            elif(getCharNumberInString(entryModify.replace(char, ""), entryModify.replace(char, "")[0]) == 2):
+                rolls[4].append([entry, number]) 
+            else:
+                rolls[5].append([entry, number]) 
+        case 1:
+            if(getCharNumberInString(entryModify.replace(char, ""), entryModify.replace(char, "")[0]) == 4):
+                rolls[1].append([entry, number]) 
+            elif(getCharNumberInString(entryModify.replace(char, ""), entryModify.replace(char, "")[0]) == 3):
+                rolls[3].append([entry, number])  
+            elif(getCharNumberInString(entryModify.replace(char, ""), entryModify.replace(char, "")[0]) == 2):
+                if(getCharNumberInString(entryModify.replace(char, ""), entryModify.replace(char, "")[0]) == 3):
+                    rolls[2].append([entry, number]) 
+                elif(getCharNumberInString(entryModify.replace(char, ""), entryModify.replace(char, "")[0]) == 2):
+                    rolls[4].append([entry, number]) 
+                else:
+                    rolls[5].append([entry, number]) 
+            else:
+                rolls[6].append([entry, number]) 
 
+      
 
 for line in fileContent.split("\n"):
+    #print(line)
     entry = line.split(" ")[0]
     number = line.split(" ")[1]
-    print(getEntryValue(entry))
+    getEntryValue(entry, number)
+
+idtem = 0
+for roll in rolls:
+    print("|------------------------" + str(idtem) + "------------------------|")
+    idtem+=1
+    for line in roll:
+        print(line)
