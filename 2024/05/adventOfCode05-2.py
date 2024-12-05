6ce811102b0b4efeeb884bd94e765caac55d94bd
Morgan-DD <111990474+Morgan-DD@users.noreply.github.com>
2024-12-05 15:51:56 +0100

    start 5.2

--- 2024/05/adventOfCode05-2.py
+++ 2024/05/adventOfCode05-2.py
@@ -1,10 +1,13 @@
 #
-# Advent of code 05-1
+# Advent of code 05-2
 #
 import os
 import sys
 import re
 import math
+from logging import NullHandler
+from os.path import split
+
 script_folder_path = os.path.dirname(os.path.abspath(sys.argv[0]))
 lineFilePath = script_folder_path + "\\data.txt"
 data = open(lineFilePath).read()
@@ -21,14 +24,19 @@ def getAllRules(number):
             allRules+=[rule]
     return allRules
 
+def getRulesForUpdate(_update):
+    _allRulesForUpdate = []
+    for _number in _update.split(","):
+        _tempRules = getAllRules(_number)
+        # print(_tempRules)
+        for _tempRule in _tempRules:
+            if _tempRule.split("|")[0] in _update and _tempRule.split("|")[1] in _update:
+                _allRulesForUpdate+=[_tempRule]
+    return _allRulesForUpdate
 
-# print(rules)
-matches = 0
-numberOfRulesTrue = 0
-areAllNumberRights = True
-
-for update in updates.split("\n"):
-    areAllNumberRights = True
+def checkLine(line):
+    isLineRight = True
+    rulesWrong = []
     for pageUpdate in update.split(","):
         matches = 0
         numberOfRulesTrue = 0
@@ -37,16 +45,54 @@ for update in updates.split("\n"):
             charZero = re.escape(singleRule.split('|')[0])
             charOne = re.escape(singleRule.split('|')[1])
             if charOne in update and charZero in update:
-                numberOfRulesTrue+=1
+                numberOfRulesTrue += 1
                 regex = r"(" + charZero + r"\,.*?" + charOne + r")"
                 if len(re.findall(regex, update)) > 0:
                     matches += 1
+                else:
+                    rulesWrong+=[singleRule]
         if not (matches == numberOfRulesTrue):
-            areAllNumberRights = False
-    if areAllNumberRights:
-        total+=int(update.split(",")[math.floor(len(update.split(","))/2)])
+            isLineRight = False
+    return isLineRight
+
+# print(rules)
+matches = 0
+numberOfRulesTrue = 0
+areAllNumberRights = True
+updateId = 0
+nbUpdates = len(updates.split("\n"))
+print("Doing Stuff...")
+rulesChecked = []
+for update in updates.split("\n"):
+    rulesChecked = []
+    print(str(updateId) + "/" + str(nbUpdates))
+    if not checkLine(update):
+        joinedNewLine = update
+        while True:
+            print("--------------")
+            rulesForUpdate = getRulesForUpdate(joinedNewLine)
+            newLine = []
+            for ruleForUpdate in rulesForUpdate:
+                if not ruleForUpdate in rulesChecked:
+                    print(rulesChecked)
+                    NoOne = ruleForUpdate.split("|")[0]
+                    NoTwo = ruleForUpdate.split("|")[1]
+                    if not NoOne in newLine and NoTwo in newLine:
+                        newLine = [NoOne] + newLine
+                    elif not NoOne in newLine:
+                        newLine+=[NoOne]
+                    if not NoTwo in newLine:
+                        newLine+=[NoTwo]
+                rulesChecked+= [ruleForUpdate]
+            joinedNewLine = ','.join(newLine)
+            if checkLine(joinedNewLine):
+                print(joinedNewLine)
+                total+=int(joinedNewLine.split(",")[math.floor(len(joinedNewLine.split(","))/2)])
+                break
+            else:
+                print(joinedNewLine + " FALSE")
+                update=joinedNewLine
 
 print("total: " + str(total))
-# < 82302
-# < 5790
-# was 5762
+# > 3974
+# > 4112
